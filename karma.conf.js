19d9b4cf27c3097bef3444a88ed87c9fef11f959
Patrick Poh <patrickpoh91@gmail.com>
2016-05-15 16:58:53 +0800

    Add automatic tests

---
+++ karma.conf.js
@@ -0,0 +1,22 @@
+var webpackConfig = require('./webpack.config.js');
+module.exports = function (config) {
+  config.set({
+    browsers: ['Chrome'],
+    singleRun: true,
+    frameworks: ['mocha'],
+    files: ['app/tests/**/*.test.jsx'],
+    preprocessors: {
+      'app/tests/**/*.test.jsx': ['webpack', 'sourcemap'] //any for the test files we want to run webpack to load in the components and use sourcemap to know which jsx files contains the error
+    },
+    reporters: ['mocha'], //which test fail which test pass
+    client: {
+      mocha: {
+        timeout: '5000'
+      }
+    },
+    webpack: webpackConfig,
+    webpackServer: {
+      noInfo: true
+    }
+  });
+};
