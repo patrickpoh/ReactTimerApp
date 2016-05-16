a884c5b9415a561501032e67727ce8794caafe9d
Patrick Poh <patrickpoh91@gmail.com>
2016-05-16 14:08:01 +0800

    Add Clock and scss

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,8 @@ module.exports = {
       Navigation: 'app/components/Navigation.jsx',
       applicationStyles: 'app/styles/app.scss',
       Countdown: 'app/components/Countdown.jsx',
-      Timer: 'app/components/Timer.jsx'
+      Timer: 'app/components/Timer.jsx',
+      Clock: 'app/components/Clock.jsx'
     },
     extensions: ['', '.js', '.jsx']
   },
