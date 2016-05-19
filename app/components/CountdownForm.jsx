9bd0b2d686f27819462512b15cb5c26936efb87a
Patrick Poh <patrickpoh91@gmail.com>
2016-05-19 10:44:12 +0800

    Commented out error Modal for test to work

--- app/components/CountdownForm.jsx
+++ app/components/CountdownForm.jsx
@@ -1,33 +1,33 @@
 var React = require('react');
-var ErrorModal = require('ErrorModal');
-var $ = require('jQuery');
+// var ErrorModal = require('ErrorModal');
+// var $ = require('jQuery');
 var CountdownForm = React.createClass({
-  getInitialState: function(){
-    return{
-      showErrorMessage: false
-    }
-  },
+  // getInitialState: function(){
+  //   return{
+  //     showErrorMessage: false
+  //   }
+  // },
   onSubmit: function(e) {
     var that = this;
     e.preventDefault();
     var strSeconds = this.refs.seconds.value;
 
     if (strSeconds.match(/^[0-9]*$/)) {
-      that.setState({
-        showErrorMessage: false
-      });
+      // that.setState({
+      //   showErrorMessage: false
+      // });
       that.refs.seconds.value = '';
       that.props.onSetCountdown(parseInt(strSeconds, 10));
     }
     else{
-      that.setState({
-        showErrorMessage: new Date().getTime()
-      });
+      // that.setState({
+      //   showErrorMessage: new Date().getTime()
+      // });
       that.refs.seconds.value = '';
     }
   },
   render: function() {
-      var {showErrorMessage} = this.state;
+      // var {showErrorMessage} = this.state;
 
       return (
         <div>
@@ -35,7 +35,7 @@ var CountdownForm = React.createClass({
             <input type="text" ref="seconds" placeholder="Enter time in seconds"/>
             <button className="button expanded">Start</button>
           </form>
-          <ErrorModal showErrorMessage={showErrorMessage}/>
+         {/*<ErrorModal showErrorMessage={showErrorMessage}/>*/}
         </div>
       );
   }
